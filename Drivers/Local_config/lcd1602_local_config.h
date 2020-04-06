ac5384d23e32d4981cf8f672d671b5b2a33f35c9
Бажен Левковець <bazhen.paseka@gmail.com>
2020-04-06 12:20:25 +0300

    add LCD1602
    add LCD1602

---
+++ Drivers/Local_config/lcd1602_local_config.h
@@ -0,0 +1,13 @@
+/*
+ * lcd1602_local_config.h
+ *
+ *  Created on: 6 ???. 2020 ?.
+ *      Author: Paseka
+ */
+
+#ifndef LOCAL_CONFIG_LCD1602_LOCAL_CONFIG_H_
+#define LOCAL_CONFIG_LCD1602_LOCAL_CONFIG_H_
+
+	#define ADR_I2C_FC113 			0x27
+
+#endif /* LOCAL_CONFIG_LCD1602_LOCAL_CONFIG_H_ */
